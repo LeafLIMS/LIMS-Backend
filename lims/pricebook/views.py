eb25c6900b307792821f7db6bcfa92cc62a80298
Thomas Craig <thomas.craig@liverpool.ac.uk>
2016-12-08 10:59:02 +0000

    Add list crm pricebooks endpoint and update pricebook fetching

--- lims/pricebook/views.py
+++ lims/pricebook/views.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+
+from simple_salesforce import Salesforce
 
 from rest_framework import viewsets
 from rest_framework.decorators import list_route
@@ -17,7 +20,24 @@ class PriceBookViewSet(AuditTrailViewMixin, viewsets.ModelViewSet):
     permission_classes = (IsInAdminGroupOrRO,)
     filter_fields = ('name', 'identifier',)
 
-    @list_route()
+    def perform_create(self, serializer):
+        serializer.save()
+        get_pricebooks()
+
+    @list_route(methods=['POST'])
     def updateall(self, request):
         get_pricebooks()
         return Response({'message': 'Pricebooks updated'})
+
+    @list_route()
+    def on_crm(self, request):
+        """
+        List of all pricebooks available on thr CRM
+        """
+        sf = Salesforce(instance_url=settings.SALESFORCE_URL,
+                        username=settings.SALESFORCE_USERNAME,
+                        password=settings.SALESFORCE_PASSWORD,
+                        security_token=settings.SALESFORCE_TOKEN)
+
+        pricebooks = sf.query("SELECT id,name FROM Pricebook2")
+        return Response(pricebooks['records'])
