f512af92dc8ac701d07c85a681ea2ca64aaad1b4
Thomas Craig <thomas.craig@liverpool.ac.uk>
2017-01-10 11:16:12 +0000

    Improve create_admin command to work with arguments

--- lims/shared/management/commands/create_admin.py
+++ lims/shared/management/commands/create_admin.py
@@ -6,10 +6,14 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     help = 'Create an admin user (if not exists) with password from env'
 
+    def add_arguments(self, parser):
+        parser.add_argument('--email', type=str, default='', dest='email')
+        parser.add_argument('--password', type=str, default='', dest='pass')
+
     def handle(self, *args, **kwargs):
 
-        admin_email = settings.SETUP_ADMIN_EMAIL
-        admin_password = settings.SETUP_ADMIN_PASSWORD
+        admin_email = kwargs['email'] if kwargs['email'] else settings.SETUP_ADMIN_EMAIL
+        admin_password = kwargs['pass'] if kwargs['pass'] else settings.SETUP_ADMIN_PASSWORD
 
         try:
             admin_user = User.objects.create_user('admin', admin_email, admin_password)
@@ -19,6 +23,7 @@ class Command(BaseCommand):
         try:
             admin_group = Group.objects.get(name='admin')
             staff_group = Group.objects.get(name='staff')
-            admin_user.groups.add(admin_group, staff_group)
+            user_group = Group.objects.get(name='user')
+            admin_user.groups.add(admin_group, staff_group, user_group)
         except:
             pass
