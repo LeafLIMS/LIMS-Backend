e379aa75690d5bacc1d0bdec325ed4c16cf1a183
Thomas Craig <thomas.craig@liverpool.ac.uk>
2017-10-13 13:17:47 +0100

    Add search functionality to permissions endpoint

--- lims/permissions/views.py
+++ lims/permissions/views.py
@@ -8,3 +8,4 @@ from .serializers import PermissionSerializer
 class PermissionViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Permission.objects.all()
     serializer_class = PermissionSerializer
+    search_fields = ('name',)
