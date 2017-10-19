81a7838b98d47245f42f01f06e54d20dc933bb23
Thomas Craig <thomas.craig@liverpool.ac.uk>
2017-10-19 14:23:20 +0100

    Update alert serializers to cover general use case

--- lims/shared/serializers.py
+++ lims/shared/serializers.py
@@ -12,24 +12,39 @@ class OrganismSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class TriggerSetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TriggerSet
-        fields = '__all__'
-
-
 class TriggerSerializer(serializers.ModelSerializer):
     triggerset_id = serializers.PrimaryKeyRelatedField(
         queryset=TriggerSet.objects.all(), source='triggerset', write_only=True)
-    triggerset = TriggerSetSerializer(read_only=True)
+    triggerset = serializers.CharField(read_only=True)#TriggerSetSerializer(read_only=True)
 
     class Meta:
         model = Trigger
         fields = '__all__'
 
 
+class TriggerSubscriptionSerializer(serializers.ModelSerializer):
+    triggerset_id = serializers.PrimaryKeyRelatedField(
+        queryset=TriggerSet.objects.all(), source='triggerset', write_only=True)
+    # triggerset = TriggerSetSerializer(read_only=True)
+    triggerset = serializers.CharField(read_only=True)
+    user = serializers.SlugRelatedField(slug_field='username', queryset=User.objects.all())
+
+    class Meta:
+        model = TriggerSubscription
+        fields = '__all__'
+
+
+class TriggerSetSerializer(serializers.ModelSerializer):
+    triggers = TriggerSerializer(many=True, read_only=True)
+    subscriptions = TriggerSubscriptionSerializer(many=True, read_only=True)
+    class Meta:
+        model = TriggerSet
+        fields = '__all__'
+
+
 class TriggerAlertSerializer(serializers.ModelSerializer):
     triggerset = TriggerSetSerializer(read_only=True)
+    # triggerset = serializers.CharField(read_only=True)
 
     class Meta:
         model = TriggerAlert
@@ -38,18 +53,13 @@ class TriggerAlertSerializer(serializers.ModelSerializer):
 
 class TriggerAlertStatusSerializer(serializers.ModelSerializer):
     triggeralert = TriggerAlertSerializer(read_only=True)
+    user = serializers.SlugRelatedField(
+        queryset=User.objects.all(),
+        slug_field='username')
+    last_updated_by = serializers.SlugRelatedField(
+        queryset=User.objects.all(),
+        slug_field='username')
 
     class Meta:
         model = TriggerAlertStatus
-        fields = '__all__'
-
-
-class TriggerSubscriptionSerializer(serializers.ModelSerializer):
-    triggerset_id = serializers.PrimaryKeyRelatedField(
-        queryset=TriggerSet.objects.all(), source='triggerset', write_only=True)
-    triggerset = TriggerSetSerializer(read_only=True)
-    user = serializers.SlugRelatedField(slug_field='username', queryset=User.objects.all())
-
-    class Meta:
-        model = TriggerSubscription
         fields = '__all__'
